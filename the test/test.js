e8dd59e64c5e3a6373cb6b03ff372bf5062e2d2f
hufo <35860681+hufome@users.noreply.github.com>
2021-08-01 13:01:32 -0700

    test

--- the test/test.js
+++ the test/test.js
@@ -1,25 +1,28 @@
+//times deleted
 var d = 0;
-var date = new Date();
-var startTime = date.getTime();
+
+var startTime = Date.now();
+var ctrl = false;
 
 document.addEventListener('keydown', function(event) {
-    
+    console.log(Date.now());
     if(event.code == "Backspace") {
         d++;
         document.getElementById("d").innerHTML = d;
+    }if(event.code == "Enter") {
+        submit(Date.now());
     }
 });
 function pasted(){
-    var pasted = true;
+    ctrl = true;
     
-    document.getElementById("d").innerHTML = pasted;
+    document.getElementById("d").innerHTML = ctrl;
 }
 function submit(time){
-      
-    finishTime = startTime - time;
-    document.getElementById("t").innerHTML = finishTime;
-    alert(document.cookie);
-    jsarray = [d, ctrl, finishTime];
-    //sessionStorage.setItem("jsArray", JSON.stringify(jsarray));
-    sessionStorage.setItem("jsArray", "test ");
+    //document.cookie = "submitted=true";
+    let input = document.getElementById("input").value
+    if (input == "chiaroscurist"){var correct = true}else{correct = false}
+    finishTime =  time - startTime;
+    jsarray = [d, ctrl, finishTime, correct];
+    document.getElementById("t").innerHTML = jsarray.toString();
 }
